0e37f03d9af7c308bfa46ea0a8f85f451cc1a734
MeilCli <ics.sora.meil@gmail.com>
2021-03-22 21:50:24 +0900

    fix message

--- src/reporter/check-run-reporter.ts
+++ src/reporter/check-run-reporter.ts
@@ -137,7 +137,7 @@ export class CheckRunReporter implements Reporter {
             const baseUrl = `https://github.com/${context.owner()}/${context.repository()}`;
             const link = `${baseUrl}/blob/${context.commitSha()}/${this.trimPath(context, lintResult.path)}#${line}`;
             result += `### [${this.trimPath(context, lintResult.path)} ${line}](${link})\n`;
-            result += `- Rule: ${lintResult.rule}\n`;
+            result += `Rule: ${lintResult.rule}\n`;
             result += lintResult.message;
             result += "\n";
         }
