7aa1fb00ef44a43c9b932ef642ede7bae3a641c3
MeilCli <ics.sora.meil@gmail.com>
2021-05-07 17:32:21 +0900

    refactor paging test code

--- __test__/github/get-commit-status-and-check-run.test.ts
+++ __test__/github/get-commit-status-and-check-run.test.ts
@@ -1,17 +1,14 @@
 import { MockedResponse, MockLink } from "@apollo/client/testing";
 import { ApolloClient, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
 import {
-    GetPullRequestChangedFile,
-    GetPullRequestChangedFileQuery,
-    GetPullRequestChangedFileQueryVariables,
     GetCommitStatusAndCheckRun,
     GetCommitStatusAndCheckRunQuery,
     GetCommitStatusAndCheckRunQueryVariables,
     StatusState,
-} from "../graphql/graphql";
-import { GitHubClient } from "../src/github/client";
-import { getCommitStatusAndCheckRunWithPaging, getPullRequestChangedFileWithPaging } from "../src/github/paging";
-import { GetCommitStatusAndCheckRunQueryCommitStatusCheckRollupContextsNode } from "../src/github/types";
+} from "../../graphql/graphql";
+import { GitHubClient } from "../../src/github/client";
+import { getCommitStatusAndCheckRunWithPaging } from "../../src/github/paging";
+import { GetCommitStatusAndCheckRunQueryCommitStatusCheckRollupContextsNode } from "../../src/github/types";
 
 function createMockClient(mocks: ReadonlyArray<MockedResponse>): ApolloClient<NormalizedCacheObject> {
     return new ApolloClient({
@@ -19,160 +16,6 @@ function createMockClient(mocks: ReadonlyArray<MockedResponse>): ApolloClient<No
         link: new MockLink(mocks, true),
     });
 }
-
-test("getPullRequestChangedFileWithPaging", async () => {
-    const mocks: MockedResponse[] = [
-        {
-            request: {
-                query: GetPullRequestChangedFile,
-                variables: {
-                    owner: "MeilCli",
-                    name: "common-lint-reporter",
-                    pull_request: 1,
-                    after: undefined,
-                } as GetPullRequestChangedFileQueryVariables,
-            },
-            result: {
-                data: {
-                    __typename: "Query",
-                    repository: {
-                        __typename: "Repository",
-                        pullRequest: {
-                            __typename: "PullRequest",
-                            files: {
-                                __typename: "PullRequestChangedFileConnection",
-                                pageInfo: {
-                                    __typename: "PageInfo",
-                                    hasNextPage: true,
-                                    endCursor: "1",
-                                },
-                                nodes: [{ __typename: "PullRequestChangedFile", path: "README.md" }],
-                            },
-                        },
-                    },
-                } as GetPullRequestChangedFileQuery,
-            },
-        },
-        {
-            request: {
-                query: GetPullRequestChangedFile,
-                variables: {
-                    owner: "MeilCli",
-                    name: "common-lint-reporter",
-                    pull_request: 1,
-                    after: "1",
-                } as GetPullRequestChangedFileQueryVariables,
-            },
-            result: {
-                data: {
-                    __typename: "Query",
-                    repository: {
-                        __typename: "Repository",
-                        pullRequest: {
-                            __typename: "PullRequest",
-                            files: {
-                                __typename: "PullRequestChangedFileConnection",
-                                pageInfo: {
-                                    __typename: "PageInfo",
-                                    hasNextPage: false,
-                                    endCursor: "2",
-                                },
-                                nodes: [{ __typename: "PullRequestChangedFile", path: "LICENSE.txt" }],
-                            },
-                        },
-                    },
-                } as GetPullRequestChangedFileQuery,
-            },
-        },
-    ];
-
-    const client = new GitHubClient(createMockClient(mocks));
-    const result = await getPullRequestChangedFileWithPaging(client, {
-        owner: "MeilCli",
-        name: "common-lint-reporter",
-        pull_request: 1,
-    });
-
-    expect(result.length).toBe(2);
-    expect(result[0].path).toBe("README.md");
-    expect(result[1].path).toBe("LICENSE.txt");
-});
-
-test("getPullRequestChangedFileWithPagingInfinityLoop", async () => {
-    const mocks: MockedResponse[] = [
-        {
-            request: {
-                query: GetPullRequestChangedFile,
-                variables: {
-                    owner: "MeilCli",
-                    name: "common-lint-reporter",
-                    pull_request: 1,
-                    after: undefined,
-                } as GetPullRequestChangedFileQueryVariables,
-            },
-            result: {
-                data: {
-                    __typename: "Query",
-                    repository: {
-                        __typename: "Repository",
-                        pullRequest: {
-                            __typename: "PullRequest",
-                            files: {
-                                __typename: "PullRequestChangedFileConnection",
-                                pageInfo: {
-                                    __typename: "PageInfo",
-                                    hasNextPage: true,
-                                    endCursor: "1",
-                                },
-                                nodes: [{ __typename: "PullRequestChangedFile", path: "README.md" }],
-                            },
-                        },
-                    },
-                } as GetPullRequestChangedFileQuery,
-            },
-        },
-        {
-            request: {
-                query: GetPullRequestChangedFile,
-                variables: {
-                    owner: "MeilCli",
-                    name: "common-lint-reporter",
-                    pull_request: 1,
-                    after: "1",
-                } as GetPullRequestChangedFileQueryVariables,
-            },
-            result: {
-                data: {
-                    __typename: "Query",
-                    repository: {
-                        __typename: "Repository",
-                        pullRequest: {
-                            __typename: "PullRequest",
-                            files: {
-                                __typename: "PullRequestChangedFileConnection",
-                                pageInfo: {
-                                    __typename: "PageInfo",
-                                    hasNextPage: true,
-                                    endCursor: "1",
-                                },
-                                nodes: [{ __typename: "PullRequestChangedFile", path: "LICENSE.txt" }],
-                            },
-                        },
-                    },
-                } as GetPullRequestChangedFileQuery,
-            },
-        },
-    ];
-
-    const client = new GitHubClient(createMockClient(mocks));
-    await expect(
-        getPullRequestChangedFileWithPaging(client, {
-            owner: "MeilCli",
-            name: "common-lint-reporter",
-            pull_request: 1,
-        })
-    ).rejects.toThrow();
-});
 
 test("getCommitStatusAndCheckRunWithPaging", async () => {
     const mocks: MockedResponse[] = [
