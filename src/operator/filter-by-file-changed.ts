62ff5c6ba95308b4f3dbee856cf89de64f987ce8
MeilCli <ics.sora.meil@gmail.com>
2021-05-09 19:07:07 +0900

    add filter by file changed operator

---
+++ src/operator/filter-by-file-changed.ts
@@ -0,0 +1,29 @@
+import * as core from "@actions/core";
+import { getOperatorOption, OperatorOption } from "./option";
+import { Operator } from "./operator";
+import { LintResult } from "../lint-result";
+
+export class FilterByFileChangedOperator extends Operator<OperatorOption> {
+    async execute(lintResults: LintResult[], option: OperatorOption): Promise<LintResult[]> {
+        const context = await this.createContext(option, lintResults, true);
+        const api = context.github.api;
+        if (api == null) {
+            return [];
+        }
+        return lintResults.filter((x) => api.changedFiles.filter((y) => y.path == x.path).length != 0);
+    }
+}
+
+async function run() {
+    try {
+        const option = getOperatorOption();
+        const operator = new FilterByFileChangedOperator();
+        await operator.operate(option);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+if (process.env.NODE_ENV != "test") {
+    run();
+}
